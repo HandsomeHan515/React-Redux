d97e3852de5c6ae91ac16414c0d0c55949a93d0d
HandsomeHan <HandsomeHan515@163.com>
2018-01-05 16:20:19 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+export const ads = [
+  {
+    "id": 1,
+    "image": "http://api.handsomehan.cn:10013/media/20111009043132953_4NWLIlb.jpg",
+    "title": "月季花",
+    "description": null,
+    "page_view": 0
+  },
+  {
+    "id": 2,
+    "image": "http://api.handsomehan.cn:10013/media/tooopen_sy_121089197226_awrU7X6.jpg",
+    "title": "花",
+    "description": null,
+    "page_view": 0
+  }
+]
